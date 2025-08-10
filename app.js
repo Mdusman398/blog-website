386961e83728731934bd715d100faca5b8f857a4
Mdusman398 <maanlive398@gmail.com>
2025-08-10 15:58:41 +0500

    updated

--- app.js
+++ app.js
@@ -16,20 +16,18 @@ function signupfnc() {
     let user = {
       signupName: signupName,
       signupEmail: signupEmail,
-      signupPassword: signupPassword
+      signupPassword: signupPassword,
     };
 
     users.push(user);
     localStorage.setItem("usersdata", JSON.stringify(users));
 
-    window.location.href= "login.html";
+    window.location.href = "login.html";
     alert("Signup successful");
   } else {
     alert("Please fill all fields");
   }
 }
-
-
 
 function loginfnc() {
   let loginEmail = document.getElementById("email").value;
@@ -53,56 +51,74 @@ function loginfnc() {
   }
 }
 
-
 // blog section
 
-
-function opendiv(){
-  let blogContainer = document.getElementById('blog-container');
-  blogContainer.style.display="flex";
-  let createBox = document.getElementById('createBox');
-  createBox.style.display="flex";
-  let body = document.getElementsByTagName('body');
+function opendiv() {
+  let blogContainer = document.getElementById("blog-container");
+  blogContainer.style.display = "flex";
+  let createBox = document.getElementById("createBox");
+  createBox.style.display = "flex";
+  let body = document.getElementsByTagName("body");
   // body.style.overflow="hidden";
   console.log(body);
 }
-function hideBox(){
-  let blogContainer = document.getElementById('blog-container');
-  blogContainer.style.display="none";
-  let createBox = document.getElementById('createBox');
-  createBox.style.display="none";
+function hideBox() {
+  let blogContainer = document.getElementById("blog-container");
+  blogContainer.style.display = "none";
+  let createBox = document.getElementById("createBox");
+  createBox.style.display = "none";
 }
 
-// function createBlog(){
-//   let blogTittleinp = document.getElementById('blogTittleinp').value
-//   let authorNameinp = document.getElementById('authorNameinp').value
-//   let blogContent = document.getElementById('blogContent').value
+function createBlog() {
+  let title_input = document.getElementById("titleInp").value;
+  let content_input = document.getElementById("contentInp").value;
+  let name_input = document.getElementById("nameInp").value;
+  let current_date = document.getElementById('dateInp').value;
 
-//   // Create blog section
-//   // blogSectin div
-//   console.log(blogTittleinp,authorNameinp,blogContent);
-//   let blogSeciton = document.createElement('div');
-//   blogSeciton.setAttribute('class','blogSeciton');
-//   let main = document.querySelector('.main');
-//   main.append(blogSeciton);
+  // when click on post the box will be ghayab
 
-//   // blog section h1
-//   let blogtittle = document.createElement('h1');
-//   blogtittle.setAttribute('id','blogtittle');
-//   blogtittle.innerText= blogTittleinp;
-//   blogSeciton.append(blogtittle);
-
-// // blog section p
-//   let blogdescription = document.createElement('p');
-//   blogdescription.setAttribute('id','blogdescription');
-//   blogdescription.innerText= blogContent;
-//   blogSeciton.append(blogdescription);
-
-// // blog section p
-//   let authorName = document.createElement('authorName');
-//   authorName.setAttribute('id','authorName');
-//   authorName.innerText= authorNameinp;
-//   authorName.append(blogdescription);
+  let blogContainer = document.getElementById("blog-container");
+  blogContainer.style.display = "none";
+  let createBox = document.getElementById("createBox");
+  createBox.style.display = "none";
+  console.log("fire");
+  // end of click on post k upr 
 
 
-// }
+
+  let blogPost = document.createElement("div");
+  blogPost.setAttribute("class", "blogPost");
+  let main = document.querySelector(".main");
+  main.append(blogPost);
+
+  // blog section h1
+  let blogtittle = document.createElement('h2');
+  blogtittle.setAttribute('class','blog-title');
+  blogtittle.innerText= title_input;
+  blogPost.append(blogtittle);
+
+
+  
+// blog section p
+  let blogContent = document.createElement('p');
+  blogContent.setAttribute('class','blog-content');
+  blogContent.innerText= content_input;
+  blogPost.append(blogContent);
+
+
+  // blog section author name
+  let authorName = document.createElement('author');
+  authorName.setAttribute('class','author');
+  authorName.innerText= name_input;
+  blogPost.append(authorName);
+
+
+  // blog section date
+
+  let blogDate = document.createElement('dateInp');
+  blogDate.setAttribute('class','date');
+  blogDate.innerText = current_date;
+  blogPost.append(blogDate)
+}
+
+
